6da7b514e2143e3067c38e185c099775957331a8
Bruno-H-Moraes <brunohmoraes93@gmail.com>
2021-01-09 16:00:48 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,17 @@
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import './App.css';
 import Routes from './Orders/Routes';
 
 function App() {
   return (
+    <>
 
      <Routes />
-    
+    <ToastContainer />
+
+    </>
   );
 }
 
